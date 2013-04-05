e4115a0739e6258e1627d5824c8cb0752cb72a53
beepscore <support@beepscore.com>
2013-04-05 14:01:45 -0700

    Add table view controller

---
+++ NamesList/BSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSTableViewController.h
+//  NamesList
+//
+//  Created by Steve Baker on 4/5/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSTableViewController : UITableViewController
+
+@end
